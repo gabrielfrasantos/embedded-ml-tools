80aafc1e51882014c1bf508b29d65288587287df
Gabriel Santos <gabriel.fra.santos@gmail.com>
2023-06-20 06:24:35 +0200

    test grandient being implemented

--- ml_tools/optimizers/test/TestGradientDescent.cpp
+++ ml_tools/optimizers/test/TestGradientDescent.cpp
@@ -4,29 +4,45 @@
 
 TEST(GradientDescentTest, validate_gradient_descent)
 {
-    constexpr std::size_t Examples = 4;
+    constexpr std::size_t Examples = 7;
     constexpr std::size_t Features = 1;
     using GradientFunctionTest = ml_tools::MeanSquaredError::GradientFunction<double, Examples, Features>;
     using CostFunctionTest = ml_tools::MeanSquaredError::CostFunction<double, Examples, Features>;
 
-    Eigen::Matrix<double, Examples, Features> x({    2.0, 
-                                                     4.0,  
-                                                     6.0,  
-                                                     8.0});
+    Eigen::Matrix<double, Examples, Features> x({    -2.5, 
+                                                     -1.0,  
+                                                      0.1,  
+                                                      1.9,
+                                                      2.7,
+                                                      3.5,
+                                                      4.3});
 
-    Eigen::Matrix<double, Examples, 1>        y({    4.5, 
-                                                     8.5, 
-                                                    12.5, 
-                                                    16.5});
+    Eigen::Matrix<double, Examples, 1>        y({    6.7, 
+                                                     4.2, 
+                                                     4.9, 
+                                                     3.84,
+                                                     3.29,
+                                                     2.81,
+                                                     2.33});
 
     GradientFunctionTest gradientFunctionTest(x, y);
     CostFunctionTest costFunctionTest(x, y);
     ml_tools::ModelParameters<double, Features> initialParameters;
 
-    initialParameters.weights << 2;
-    initialParameters.bias << 0.5;
+    initialParameters.weights << -0.6;
+    initialParameters.bias << 4.5;
 
-    ml_tools::GradientDescent<double, GradientFunctionTest, CostFunctionTest, Features> gd(gradientFunctionTest, costFunctionTest, 1, 1, 0.001, 0);
+    ml_tools::GradientDescent<double, GradientFunctionTest, CostFunctionTest, Features> gd(gradientFunctionTest, costFunctionTest, 25, 5, 0.01, 0);
 
-    gd.Minimize(initialParameters, [](std::size_t epoch, double loss, ml_tools::ModelParameters<double, Features>& grandient){}, [](ml_tools::ModelParameters<double, Features>&){});
+    auto onEpoch = [](std::size_t epoch, double loss, ml_tools::ModelParameters<double, Features>& grandient)
+    {
+        std::cerr << "epoch: " << epoch << ", loss: " << loss << std::endl;
+    };
+
+    auto onDone = [](ml_tools::ModelParameters<double, Features>&)
+    {
+
+    };
+
+    gd.Minimize(initialParameters, onEpoch, onDone);
 }
